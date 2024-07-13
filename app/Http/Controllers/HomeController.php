7378c411f6dbae25715b2d7b01167f203eb459a4
Jeanpier19 <jeanspierm.19jt@gmail.com>
2024-07-12 19:40:47 -0500

    whatsapp por escuelas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,6 +46,44 @@ class HomeController extends Controller
         $convenios_no_vigentes = Convenio::where('estado', 'NO VIGENTE')->count();
         $convenios_por_finalizar = Convenio::where('dias_restantes', '<=', 30)->count();
         $convenios = Convenio::count();
-        return view('home', compact('cantidad_egresados', 'cantidad_graduados', 'cantidad_titulados', 'cantidad_postgrado', 'cantidad_maestria', 'cantidad_doctorados', 'convenios', 'cantidad_ofertas_laborales', 'convenios_vigentes', 'convenios_no_vigentes', 'convenios_por_finalizar'));
+        $usuario = Auth::user()->id;
+
+        $escuela = Alumno::join('users', 'users.id', '=', 'alumno.user_id')
+            ->join('egresados', 'egresados.alumnos_id', '=', 'alumno.id')
+            ->join('escuela', 'escuela.id', '=', 'egresados.escuela_id')
+            ->where('users.id', $usuario)
+            ->select('escuela.nombre')
+            ->first(); // Use first() to get a single result instead of get()
+
+        $escuela_nombre = $escuela ? $escuela->nombre : null; // Get the name or null if no result
+
+        $grupos_whatsapp = [
+            'ADMINISTRACIÓN' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'TURISMO' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'ESTADÍSTICA E INFORMÁTICA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'MATEMÁTICA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'INGENIERÍA DE SISTEMAS E INFORMÁTICA' => 'https://chat.whatsapp.com/CS16fH3ks5JD36n72Y0RZT',
+            'AGRONOMÍA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'INGENIERÍA AGRÍCOLA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'INGENIERÍA AMBIENTAL' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'INGENIERÍA SANITARIA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'ENFERMERÍA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'OBSTETRICIA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'ARQUEOLOGÍA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'CIENCIAS DE LA COMUNICACIÓN' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'EDUCACIÓN ESPECIALIDAD DE COMUNICACIÓN LINGÜÍSTICA Y LITERATURA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'EDUCACIÓN ESPECIALIDAD DE LENGUA EXTRANJERA: INGLÉS' => 'https://chat.whatsapp.com/KppDWxStHL01Y9ISvBjm0c',
+            'EDUCACIÓN ESPECIALIDAD DE PRIMARIA Y EDUCACIÓN BILINGÜE INTERCULTURAL' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'EDUCACIÓN SECUNDARIA ESPECIALIDAD DE MATEMÁTICA E INFORMÁTICA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'DERECHO Y CIENCIAS POLÍTICAS' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'CONTABILIDAD' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'ECONOMÍA' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'INGENIERÍA CIVIL' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'ARQUITECTURA Y URBANISMO' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'INGENIERÍA DE INDUSTRIAS ALIMENTARIAS' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'INGENIERÍA INDUSTRIAL' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+            'INGENIERÍA DE MINAS' => 'https://chat.whatsapp.com/tuCodigoDeInvitacion',
+        ];
+        return view('home', compact('escuela_nombre', 'usuario', 'grupos_whatsapp', 'cantidad_egresados', 'cantidad_graduados', 'cantidad_titulados', 'cantidad_postgrado', 'cantidad_maestria', 'cantidad_doctorados', 'convenios', 'cantidad_ofertas_laborales', 'convenios_vigentes', 'convenios_no_vigentes', 'convenios_por_finalizar'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,56 @@
 @extends('layouts.app')
 
+@section('css')
+    <style>
+        .whatsapp-bar {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            z-index: 1000;
+            background-color: #25D366;
+            /* Color de fondo de WhatsApp */
+            border-radius: 50px;
+            padding: 10px 20px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            display: flex;
+            align-items: center;
+        }
+
+        .whatsapp-bar:hover {
+            background-color: #128C7E;
+        }
+
+        .whatsapp-bar a:hover,
+        .whatsapp-bar a i:hover {
+            color: #fff;
+        }
+
+        .whatsapp-bar a:focus,
+        .whatsapp-bar a i:focus {
+            color: #000;
+        }
+
+        .whatsapp-link {
+            display: flex;
+            align-items: center;
+            font-size: 18px;
+            color: #fff;
+            /* Color del texto */
+            text-decoration: none;
+        }
+
+        .whatsapp-link i {
+            font-size: 24px;
+            margin-right: 10px;
+        }
+
+        .whatsapp-link:hover {
+            /* Color al pasar el ratón */
+            border-radius: 50px;
+        }
+    </style>
+@endsection
+
 @section('content')
     <div class="container-fluid">
         @if ($message = Session::get('success'))
@@ -12,138 +63,151 @@
         @endif
         @switch (Auth::user()->getRoleNames()[0])
             @case('Administrador')
-            <div class="row">
-                <div class="col-xs-6">
-                    <div class="col-sm-6">
-                        <article class="statistic-box red">
-                            <div>
-                                <div class="number">{{$cantidad_egresados}}</div>
-                                <div class="caption">
-                                    <div>Egresados Pre Grado</div>
+                <div class="row">
+                    <div class="col-xs-6">
+                        <div class="col-sm-6">
+                            <article class="statistic-box red">
+                                <div>
+                                    <div class="number">{{ $cantidad_egresados }}</div>
+                                    <div class="caption">
+                                        <div>Egresados Pre Grado</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                    </div><!--.col-->
-                    <div class="col-sm-6">
-                        <article class="statistic-box purple">
-                            <div>
-                                <div class="number">{{$cantidad_graduados}}</div>
-                                <div class="caption">
-                                    <div>Bachiller</div>
+                            </article>
+                        </div><!--.col-->
+                        <div class="col-sm-6">
+                            <article class="statistic-box purple">
+                                <div>
+                                    <div class="number">{{ $cantidad_graduados }}</div>
+                                    <div class="caption">
+                                        <div>Bachiller</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                    </div><!--.col-->
-                    <div class="col-sm-6">
-                        <article class="statistic-box yellow">
-                            <div>
-                                <div class="number">{{$cantidad_titulados}}</div>
-                                <div class="caption">
-                                    <div>Titulados</div>
+                            </article>
+                        </div><!--.col-->
+                        <div class="col-sm-6">
+                            <article class="statistic-box yellow">
+                                <div>
+                                    <div class="number">{{ $cantidad_titulados }}</div>
+                                    <div class="caption">
+                                        <div>Titulados</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                    </div><!--.col-->
-                    <div class="col-sm-6">
-                        <article class="statistic-box blue">
-                            <div>
-                                <div class="number">{{$cantidad_postgrado}}</div>
-                                <div class="caption">
-                                    <div>Egresados Postgrado</div>
+                            </article>
+                        </div><!--.col-->
+                        <div class="col-sm-6">
+                            <article class="statistic-box blue">
+                                <div>
+                                    <div class="number">{{ $cantidad_postgrado }}</div>
+                                    <div class="caption">
+                                        <div>Egresados Postgrado</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                    </div><!--.col-->
-                    <div class="col-sm-6">
-                        <article class="statistic-box green">
-                            <div>
-                                <div class="number">{{$cantidad_maestria}}</div>
-                                <div class="caption">
-                                    <div>Maestría</div>
+                            </article>
+                        </div><!--.col-->
+                        <div class="col-sm-6">
+                            <article class="statistic-box green">
+                                <div>
+                                    <div class="number">{{ $cantidad_maestria }}</div>
+                                    <div class="caption">
+                                        <div>Maestría</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                    </div><!--.col-->
-                    <div class="col-sm-6">
-                        <article class="statistic-box teal">
-                            <div>
-                                <div class="number">{{$cantidad_doctorados}}</div>
-                                <div class="caption">
-                                    <div>Doctorado</div>
+                            </article>
+                        </div><!--.col-->
+                        <div class="col-sm-6">
+                            <article class="statistic-box teal">
+                                <div>
+                                    <div class="number">{{ $cantidad_doctorados }}</div>
+                                    <div class="caption">
+                                        <div>Doctorado</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                    </div><!--.col-->
-                    <div class="col-sm-6">
-                        <article class="statistic-box orange">
-                            <div>
-                                <div class="number">{{$cantidad_ofertas_laborales}}</div>
-                                <div class="caption">
-                                    <div>Ofertas laborales</div>
+                            </article>
+                        </div><!--.col-->
+                        <div class="col-sm-6">
+                            <article class="statistic-box orange">
+                                <div>
+                                    <div class="number">{{ $cantidad_ofertas_laborales }}</div>
+                                    <div class="caption">
+                                        <div>Ofertas laborales</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </article>
-                    </div><!--.col-->
-                </div>
-                <div class="col-xs-6">
-                    <section class="widget widget-pie-chart">
-                        <div class="no-padding">
-                            <div class="tbl tbl-grid text-center">
-                                <div class="tbl-row">
-                                    <div class="tbl-cell tbl-cell-50">
-                                        <div class="display-inline">
-                                            <div class="chart-legend">
-                                                <div class="circle green"></div>
-                                                <div class="percent">{{$convenios_vigentes}}</div>
-                                                <div class="caption">Vigentes</div>
+                            </article>
+                        </div><!--.col-->
+                    </div>
+                    <div class="col-xs-6">
+                        <section class="widget widget-pie-chart">
+                            <div class="no-padding">
+                                <div class="tbl tbl-grid text-center">
+                                    <div class="tbl-row">
+                                        <div class="tbl-cell tbl-cell-50">
+                                            <div class="display-inline">
+                                                <div class="chart-legend">
+                                                    <div class="circle green"></div>
+                                                    <div class="percent">{{ $convenios_vigentes }}</div>
+                                                    <div class="caption">Vigentes</div>
+                                                </div>
+                                                <div class="chart-legend">
+                                                    <div class="circle orange"></div>
+                                                    <div class="percent">{{ $convenios_por_finalizar }}</div>
+                                                    <div class="caption">Por finalizar</div>
+                                                </div>
+                                                <div class="chart-legend">
+                                                    <div class="circle red"></div>
+                                                    <div class="percent">{{ $convenios_no_vigentes }}</div>
+                                                    <div class="caption">No vigentes</div>
+                                                </div>
                                             </div>
-                                            <div class="chart-legend">
-                                                <div class="circle orange"></div>
-                                                <div class="percent">{{$convenios_por_finalizar}}</div>
-                                                <div class="caption">Por finalizar</div>
-                                            </div>
-                                            <div class="chart-legend">
-                                                <div class="circle red"></div>
-                                                <div class="percent">{{$convenios_no_vigentes}}</div>
-                                                <div class="caption">No vigentes</div>
+                                        </div>
+                                        <div class="tbl-cell tbl-cell-50">
+                                            <div class="ggl-pie-chart-container">
+                                                <div id="donutchart" class="ggl-pie-chart"></div>
+                                                <div class="ggl-pie-chart-title">
+                                                    <div class="caption">Total</div>
+                                                    <div class="number">{{ $convenios }}</div>
+                                                    <div class="caption"><a href="{{ route('convenios.index') }}">Convenios</a>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="tbl-cell tbl-cell-50">
-                                        <div class="ggl-pie-chart-container">
-                                            <div id="donutchart" class="ggl-pie-chart"></div>
-                                            <div class="ggl-pie-chart-title">
-                                                <div class="caption">Total</div>
-                                                <div class="number">{{$convenios}}</div>
-                                                <div class="caption"><a
-                                                        href="{{route('convenios.index')}}">Convenios</a></div>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </section><!--.widget-pie-chart-->
+                        </section><!--.widget-pie-chart-->
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-            </div>
+                <div class="row">
+                </div>
             @break
+
             @case('Egresado')
                 <div class="col-xs-6">
                     <div class="col-sm-6">
                         <article class="statistic-box green">
                             <div>
-                                <div class="number">{{$cantidad_ofertas_laborales}}</div>
+                                <div class="number">{{ $cantidad_ofertas_laborales }}</div>
                                 <div class="caption">
                                     <div>Ofertas laborales</div>
                                 </div>
                             </div>
                         </article>
                     </div><!--.col-->
+                    @if ($escuela_nombre)
+                        @foreach ($grupos_whatsapp as $grupo => $enlace)
+                            @if ($grupo === $escuela_nombre)
+                                <div class="whatsapp-bar">
+                                    <a href="{{ $enlace }}" target="_blank" class="whatsapp-link">
+                                        <i class="fab fa-whatsapp"></i> WhatsApp
+                                    </a>
+                                </div>
+                            @endif
+                        @endforeach
+                    @else
+                        <p>No se encontró la escuela del usuario.</p>
+                    @endif
                 </div>
             @break
         @endswitch
     </div>
 @endsection
-
