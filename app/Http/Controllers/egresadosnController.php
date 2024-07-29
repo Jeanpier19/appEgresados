bccd359cf86dd3aa16dcf5fc54e59279d81bd40f
Jeanpier19 <jeanspierm.19jt@gmail.com>
2024-07-29 13:02:33 -0500

    Subida masiva de egresados

--- app/Http/Controllers/egresadosnController.php
+++ app/Http/Controllers/egresadosnController.php
@@ -46,8 +46,8 @@ class egresadosnController extends Controller
                 'egresados.codigo_local'
             )
             ->get();
-        $ciclos = ['Seleccionar', '1', '2'];
-        $genero = ['Seleccionar','MASCULINO', 'FEMENINO'];
+        $ciclos = ['Seleccionar', '1', '2', '0'];
+        $genero = ['Seleccionar', 'MASCULINO', 'FEMENINO'];
         $tip_doc = ['DNI', 'PASAPORTE'];
         $facultades = Facultad::select('id', 'nombre')->get();
         $escuelas = Escuela::select('id', 'nombre')->get();
